dcf75a513d2dba1c9c3e12a9d748f04ae8bd0f44
JefersonDuarte <jeferson_duartemk@outlook.com>
2021-08-26 16:20:41 -0300

    feat(app): implements setStates function

--- api-ibge/src/App.js
+++ api-ibge/src/App.js
@@ -1,11 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <h1 className="title">Hello World</h1>
-    </div>
-  );
+const App = () => {
+
+  document.title = 'API IBGE';
+
+  const [estados, setEstados] = useState([]);
+  const [cidades, selectCidades] = useState([{ id: 1, nome: 'foo' }]);
+
+  useEffect(async () => {
+    const response = await fetch('https://servicodados.ibge.gov.br/api/v1/localidades/estados');
+    const data = await response.json();
+
+    data.sort((a, b) =>
+      a.sigla > b.sigla ? 1 : b.sigla > a.sigla ? -1 : 0
+    );
+
+    setEstados(data);
+
+  }, []);
+
+return (
+  <div className="App">
+    <h1 className="title">Preencha seu endereço:</h1>
+    <form>
+
+    </form>
+  </div>
+);
 }
 
 export default App;
